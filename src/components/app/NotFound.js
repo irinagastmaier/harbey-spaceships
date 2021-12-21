edebfcae68f2e9cd17df52697a0baabb63cf5c0d
Irina <irinagmarques@hotmail.com>
2021-12-21 21:55:15 +0100

    routing

--- src/components/app/NotFound.js
+++ src/components/app/NotFound.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Button } from '@chakra-ui/react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
-    return (
-        <div>
-            <h1>Page not found</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Page not found</h1>
+      <Link to="/">
+        <Button>Go to Home</Button>
+      </Link>
+    </div>
+  );
 }
