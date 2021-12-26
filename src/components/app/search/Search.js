e03203f2f16a9bdbccce3ed11176078bfdaedb35
Irina <irinagmarques@hotmail.com>
2021-12-26 19:09:45 +0100

    update search bar layout

--- src/components/app/search/Search.js
+++ src/components/app/search/Search.js
@@ -2,6 +2,15 @@ import { useQuery } from '@apollo/client';
 import { SHIP_SEARCH_QUERY } from '../../../services/queries.js';
 import useShipFilters from './helpers.js';
 import Loading from '../../common/Loading';
+import {
+  Button,
+  Icon,
+  Input,
+  InputGroup,
+  InputRightElement,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { FaSearch } from 'react-icons/fa';
 
 const Search = () => {
   const { data, loading, error, refetch } = useQuery(SHIP_SEARCH_QUERY);
@@ -9,34 +18,41 @@ const Search = () => {
 
   if (loading) return <Loading />;
   if (error) return <div>error</div>;
-  console.log(error);
 
   return (
     <div>
-      <div>
-        <div>
-          <label>Search</label>
-          <input
-            //add type and missions
-            onChange={(e) => operations.updateFilter('name', e.target.value)}
-            type="string"
+      <InputGroup size="md" w="60%" m="0 auto">
+        <Input
+          focusBorderColor="teal.700"
+          variant="filled"
+          borderColor="gray.200"
+          //add type and missions
+          onChange={(e) => operations.updateFilter('name', e.target.value)}
+          type="string"
+          placeholder="Enter ship: name, type or mission"
+        />
+        <InputRightElement width="4.5rem">
+          <Icon
+            as={FaSearch}
+            h={5}
+            w={5}
+            ml={3}
+            _hover={{
+              color: '#487999',
+            }}
+            onClick={() =>
+              refetch({
+                shipsInput: {
+                  name: models.filters.name,
+                  type: models.filters.type,
+                  // mission: models.filters.mission_name,
+                },
+              })
+            }
           />
-        </div>
-        <button
-          onClick={() =>
-            refetch({
-              shipsInput: {
-                name: models.filters.name,
-                type: models.filters.type,
-                // mission: models.filters.mission_name,
-              },
-            })
-          }
-        >
-          Submit!
-        </button>
-      </div>
-      <div>
+        </InputRightElement>
+      </InputGroup>
+      {/* <div>
         {data.ships.map((ship, i) => (
           <div key={i}>
             <h1 style={{ color: 'red' }}>{ship.name} </h1>
@@ -47,7 +63,7 @@ const Search = () => {
             ))}
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
