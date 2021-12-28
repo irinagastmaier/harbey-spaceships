54b17cc7c55b968dd00b743688df0e26d6df4ed4
Irina <irinagmarques@hotmail.com>
2021-12-28 19:36:28 +0100

    layout minor fixes

--- src/components/favourite/Favourite.js
+++ src/components/favourite/Favourite.js
@@ -5,6 +5,7 @@ import { FaTrash } from 'react-icons/fa';
 export default function Favourite() {
   const dispatch = useDispatch();
   const favourite = useSelector((state) => state.favouriteReducer.favourite);
+  const quantity = useSelector((state) => state.favouriteReducer.quantity);
 
   return (
     <>
@@ -27,34 +28,41 @@ export default function Favourite() {
           <Heading as="h1" size="xl" isTruncated p={6}>
             Your Favourites
           </Heading>
-          <Box boxShadow={'2xl'} rounded={'md'} p={6} overflow={'hidden'}>
-            {favourite.map((ship, i) => {
-              return (
-                <Box key={i} w="100%">
-                  <Text pl={4} pb={4}>
-                    {ship.name}
-                    <Icon
-                      as={FaTrash}
-                      ml={2}
-                      h={3}
-                      w={3}
-                      alignSelf={'center'}
-                      color="gray.400"
-                      _hover={{
-                        color: 'red.500',
-                      }}
-                      onDoubleClick={() =>
-                        dispatch({
-                          type: 'REMOVE_FROM_FAVOURITE',
-                          payload: ship,
-                        })
-                      }
-                    />
-                  </Text>
-                </Box>
-              );
-            })}
-          </Box>
+          {quantity >= 1 ? (
+            <Box boxShadow={'2xl'} rounded={'md'} p={6} overflow={'hidden'}>
+              {favourite.map((ship, i) => {
+                return (
+                  <Box key={i} w="100%">
+                    <Text pl={4} pb={4}>
+                      {ship.name}
+                      <Icon
+                        as={FaTrash}
+                        ml={2}
+                        h={3}
+                        w={3}
+                        alignSelf={'center'}
+                        color="gray.400"
+                        cursor="pointer"
+                        _hover={{
+                          color: 'red.500',
+                        }}
+                        onDoubleClick={() =>
+                          dispatch({
+                            type: 'REMOVE_FROM_FAVOURITE',
+                            payload: ship,
+                          })
+                        }
+                      />
+                    </Text>
+                  </Box>
+                );
+              })}
+            </Box>
+          ) : (
+            <Text pl={6} pb={2}>
+              No favourites saved
+            </Text>
+          )}
         </Box>
       </Flex>
     </>
